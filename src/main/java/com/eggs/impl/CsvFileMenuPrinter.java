d25fed15dff68e986b58accaac70a28d00ca4910
CSABA_VALYI <Csaba_Valyi@epam.com>
2013-10-18 13:19:19 +0200

    [Csabi] Reading csv files from the resources folder implemented

--- src/main/java/com/eggs/impl/CsvFileMenuPrinter.java
+++ src/main/java/com/eggs/impl/CsvFileMenuPrinter.java
@@ -1,6 +1,7 @@
 package com.eggs.impl;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -15,6 +16,37 @@ public class CsvFileMenuPrinter implements MenuRepository {
 	private List<Menu> menus = new ArrayList<Menu>();
 	
 	public CsvFileMenuPrinter(String[] args){
+		
+		File folder = new File("./src/main/resources");
+		File[] listOfFiles = folder.listFiles();
+		for(File thereIsMore : listOfFiles){
+			System.out.format("%s%n", thereIsMore.getName());
+		}
+		
+		folder.delete();
+		//Building menus from every csv files found in the resources folder
+		for(File file : listOfFiles){
+			if(file.getName().contains(".csv")){
+				try{
+					BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(file.getName())));
+					String rest = file.getName().replace(".csv","");
+					MenuBuilder mb = MenuBuilder.menu()
+							.restaurant(rest);
+					String line;			
+					line = br.readLine();
+					while(line != null){
+						mb.food(line.split(",")[0], line.split(",")[1], Float.parseFloat(line.split(",")[2]));
+						line = br.readLine();
+					}			
+					menus.add(mb.build());
+					br.close();
+				}catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		//Building menus from the csv files given as arguments
 		for(String file : args){
 			try {
 				BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(file)));
@@ -23,9 +55,7 @@ public class CsvFileMenuPrinter implements MenuRepository {
 				MenuBuilder mb = MenuBuilder.menu()
 								.restaurant(rest);
 				String line;			
-				line = br.readLine();			
-				String menu="";
-				
+				line = br.readLine();
 				while(line != null){
 					mb.food(line.split(",")[0], line.split(",")[1], Float.parseFloat(line.split(",")[2]));
 					line = br.readLine();
@@ -36,8 +66,7 @@ public class CsvFileMenuPrinter implements MenuRepository {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		
+		}		
 	}
 	public List<Menu> getAllmenu(){
 		return menus;
