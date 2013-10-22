5ecc0b91153a554bc61ece9280ec718a7dcd423b
lalyos <lalyos@yahoo.com>
2013-10-22 16:28:25 +0200

    property name 'format' renamed to 'date.format'

--- src/main/java/com/eggs/GoHome.java
+++ src/main/java/com/eggs/GoHome.java
@@ -9,7 +9,7 @@ public class GoHome {
 
     private static final String GOHOME_MSG = "gohome.msg";
     private static final String GOHOME_DATE = "gohome.date";
-    private static final String GOHOME_FORMAT = "gohome.format";
+    private static final String GOHOME_DATE_FORMAT = "gohome.date.format";
     private static final String GOHOME_PROPERTIES = "gohome.properties";
 
     public static void main(String[] args) throws Exception {
@@ -18,11 +18,11 @@ public class GoHome {
         props.load(GoHome.class.getClassLoader().getResourceAsStream(GOHOME_PROPERTIES));
         
         Date now = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat(props.getProperty(GOHOME_FORMAT));
+        SimpleDateFormat sdf = new SimpleDateFormat(props.getProperty(GOHOME_DATE_FORMAT));
         Date gohome = sdf.parse(props.getProperty(GOHOME_DATE));
         
         long minutes = (gohome.getTime() - now.getTime()) / 60000;      
-        String msg = props.getProperty(GOHOME_MSG);
+        String format = props.getProperty(GOHOME_MSG);
         System.out.println(msg + minutes);
     }
 
