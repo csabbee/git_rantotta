d8c6035f77768b830e61c546e4927c0eecea9c32
lalyos <lalyos@yahoo.com>
2013-10-22 11:24:22 +0200

    YAML reader instead of CSV readed

--- src/main/java/com/eggs/App.java
+++ src/main/java/com/eggs/App.java
@@ -2,10 +2,12 @@ package com.eggs;
 
 import com.eggs.impl.ConsoleMenuPrinter;
 import com.eggs.impl.CsvFileMenuRepositoryReader;
+import com.eggs.impl.YamlFileMenuRepositoryReader;
 
 public class App {
 	public static void main(String[] args) {
-		CsvFileMenuRepositoryReader reader = new CsvFileMenuRepositoryReader("karcsi", "marcello");
+//		CsvFileMenuRepositoryReader reader = new CsvFileMenuRepositoryReader("karcsi", "marcello");
+		YamlFileMenuRepositoryReader reader = new YamlFileMenuRepositoryReader("menus.yml");
 		ConsoleMenuPrinter printer = new ConsoleMenuPrinter(reader.read());
 		
 	    printer.printMenus();
