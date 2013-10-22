5a26616fd2a85d94c357dddbc86b20bd07f19edc
lalyos <lalyos@yahoo.com>
2013-10-22 11:52:23 +0200

    log4j added to the total logging caos

--- src/main/java/com/eggs/App.java
+++ src/main/java/com/eggs/App.java
@@ -1,15 +1,29 @@
 package com.eggs;
 
+import org.apache.log4j.Logger;
+
 import com.eggs.impl.ConsoleMenuPrinter;
 import com.eggs.impl.CsvFileMenuRepositoryReader;
 import com.eggs.impl.YamlFileMenuRepositoryReader;
 
 public class App {
-	public static void main(String[] args) {
-//		CsvFileMenuRepositoryReader reader = new CsvFileMenuRepositoryReader("karcsi", "marcello");
-		YamlFileMenuRepositoryReader reader = new YamlFileMenuRepositoryReader("menus.yml");
+	private static Logger logger = Logger.getLogger(App.class);
+	
+	private static void printReader(MenuRepositoryReader reader) {
 		ConsoleMenuPrinter printer = new ConsoleMenuPrinter(reader.read());
 		
 	    printer.printMenus();
+	}
+	
+	public static void main(String[] args) {
+		
+		String fileName = args[0];
+		logger.info("reading menu from:" + fileName);
+		
+		CsvFileMenuRepositoryReader csvReader = new CsvFileMenuRepositoryReader("karcsi", "marcello");
+		YamlFileMenuRepositoryReader yamlReader = new YamlFileMenuRepositoryReader(fileName);
+
+		printReader(csvReader);
+		printReader(yamlReader);		
 	}
 }
