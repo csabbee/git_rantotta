9199942b891a593b4c7fc323080f00e1757076e7
lalyos <lalyos@yahoo.com>
2013-10-24 11:20:49 +0200

    adding loop to test the debugger breakpoint

--- src/main/java/com/eggs/I18NTest.java
+++ src/main/java/com/eggs/I18NTest.java
@@ -3,10 +3,37 @@ package com.eggs;
 
 public class I18NTest {
 
+    
     public static void main(String[] args) {
-        //System.out.println("yooo");
         System.out.println("\u00c1rv\u00edzt\u0171r\u0151 t\u00fck\u00f6rf\u00far\u00f3g\u00e9p");
+        
+        I18NTest app = new I18NTest();
+        app.loop();
+        
+    }
 
+    private void loop() {
+        boolean notend = true;
+        while(notend) {
+            sleep();
+            printSomething();
+        }
+        
+    }
+
+    private void printSomething() {
+        System.out.println("ebed");
+        
+    }
+
+    private void sleep() {
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 
 }
