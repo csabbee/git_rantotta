74329d9d1240e15de7b71f2fc8deddd200ecbb20
lalyos <lalyos@yahoo.com>
2013-10-24 10:50:04 +0200

    [I18N] arvizturo test

---
+++ src/main/java/com/eggs/I18NTest.java
@@ -0,0 +1,12 @@
+package com.eggs;
+
+
+public class I18NTest {
+
+    public static void main(String[] args) {
+        //System.out.println("yooo");
+        System.out.println("\u00c1rv\u00edzt\u0171r\u0151 t\u00fck\u00f6rf\u00far\u00f3g\u00e9p");
+
+    }
+
+}
