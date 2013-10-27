a228f028f4b7ba8f8cf01013a5afa377692c4be6
Csaba Valyi <csaba.valy1@gmail.com>
2013-10-27 12:53:05 +0100

    on the fly menutranslation implemented

--- src/main/java/com/eggs/impl/ConsoleMenuPrinter.java
+++ src/main/java/com/eggs/impl/ConsoleMenuPrinter.java
@@ -1,9 +1,11 @@
 package com.eggs.impl;
 
+import com.eggs.BaseMenuPrinter;
 import com.eggs.Food;
 import com.eggs.Menu;
-import com.eggs.BaseMenuPrinter;
 import com.eggs.MenuRepository;
+import com.gtranslate.Language;
+import com.gtranslate.Translator;
 
 public class ConsoleMenuPrinter extends BaseMenuPrinter {
 
@@ -13,9 +15,10 @@ public class ConsoleMenuPrinter extends BaseMenuPrinter {
 
     @Override
     protected void printSingleMenu(Menu menu) {
+    	Translator translate = Translator.getInstance();
         System.out.format("%n=== %s ===%n", menu.getRestaurant().toString());
         for (Food food : menu.getFoodList()) {
-            System.out.format(" - %-20s : %10.2f %n", food.getName(), food.getPrice());
+            System.out.format(" - %-20s : %10.2f %n", translate.translate(food.getName(), Language.HUNGARIAN, Language.FRENCH), food.getPrice());
         }
     }
 
