81b0578b2183e6ac066613ee3f0e4f2468d58e8c
lalyos <lalyos@yahoo.com>
2013-11-04 14:21:24 +0100

    ApplicationContext introduced

--- src/main/java/com/eggs/App.java
+++ src/main/java/com/eggs/App.java
@@ -1,12 +1,16 @@
 package com.eggs;
 
+import java.applet.AppletContext;
 import java.util.logging.LogManager;
 
 import org.apache.log4j.Logger;
+import org.omg.CORBA.portable.ApplicationException;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 import com.eggs.impl.ConsoleMenuPrinter;
@@ -32,12 +36,13 @@ public class App {
         String fileName = args[0];
         logger.info("reading menu from:" + fileName);
 
-        ListableBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        //ListableBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         
-        MenuPrinter printer = factory.getBean(MenuPrinter.class);
+        MenuPrinter printer = ctx.getBean(MenuPrinter.class);
         printer.printMenus();
         
-        String[] beanNamse = factory.getBeanDefinitionNames();
+        String[] beanNamse = ctx.getBeanDefinitionNames();
         for (String name : beanNamse) {
             
             System.out.println("bean name:" + name);
