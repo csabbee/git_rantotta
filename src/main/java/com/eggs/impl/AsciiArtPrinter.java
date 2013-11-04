5aa41e41acdf759fe6223ba5856cbee7bb808b75
lalyos <lalyos@yahoo.com>
2013-11-04 17:40:00 +0100

    asciiPrinter added based on http://artii.herokuapp.com/

---
+++ src/main/java/com/eggs/impl/AsciiArtPrinter.java
@@ -0,0 +1,77 @@
+package com.eggs.impl;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import com.eggs.BaseMenuPrinter;
+import com.eggs.Food;
+import com.eggs.Menu;
+import com.eggs.MenuRepositoryReader;
+
+public class AsciiArtPrinter extends BaseMenuPrinter implements ApplicationContextAware {
+
+    private Logger logger = LoggerFactory.getLogger(AsciiArtPrinter.class);
+    
+    private ApplicationContext ctx;
+    // shadow,lean,standard,starwars,speed,shadow,nipples,lean,big,
+    private String font = "doom";
+    
+    public AsciiArtPrinter() {
+        super();
+    }
+    public AsciiArtPrinter(MenuRepositoryReader reader) {
+    }
+    
+    private String getAsciiText(String msg) {
+        StringBuilder ret = new StringBuilder();
+        try {
+            URL url = new URL("http://artii.herokuapp.com/make?text=" + msg + "&font=" + this.font);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while ( (line = reader.readLine()) != null ) {
+                logger.debug("next line: {}", line);
+                ret.append(line + "\n");
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret.toString();
+    }
+
+    @Override
+    protected void printSingleMenu(Menu menu) {
+        String name = menu.getRestaurant().getName();
+        System.out.println(getAsciiText(name));
+        
+        
+        
+        String nameHeader = ctx.getMessage("header.name", new Object[0], Locale.getDefault());
+        String priceHeader = ctx.getMessage("header.price", new Object[0],  Locale.getDefault());
+        
+        System.out.format(" %-20s | %10s %n", nameHeader, priceHeader);
+        
+        for (Food food : menu.getFoodList()) {
+            System.out.format(" %-20s | %10.2f %n", food.getName(), food.getPrice());
+        }
+    }
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.ctx = ctx;
+        
+    }
+    public String getFont() {
+        return font;
+    }
+    public void setFont(String font) {
+        this.font = font;
+    }
+
+}
