a6bc08514b0a9b45acee56902741ed707765be0e
lalyos <lalyos@yahoo.com>
2013-11-05 17:17:23 +0100

    Adding a new FoodEvent Listener

---
+++ src/main/java/com/eggs/VegetarianMenuSelector.java
@@ -0,0 +1,17 @@
+package com.eggs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VegetarianMenuSelector implements ApplicationListener<FoodEvent>{
+
+    private static final Logger logger = LoggerFactory.getLogger(VegetarianMenuSelector.class);
+    public void onApplicationEvent(FoodEvent event) {
+        logger.error("pleace decide weather {} is vegetarian !", event.getFood().getName());
+        
+    }
+
+}
