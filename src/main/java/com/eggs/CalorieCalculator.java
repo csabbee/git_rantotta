2cae71b43b0d57f3c91ed5eb07cf1c94a4bd4864
lalyos <lalyos@yahoo.com>
2013-11-05 17:09:23 +0100

    CalorieCalculator Listener added

---
+++ src/main/java/com/eggs/CalorieCalculator.java
@@ -0,0 +1,17 @@
+package com.eggs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalorieCalculator implements ApplicationListener<FoodEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(CalorieCalculator.class);
+    
+    public void onApplicationEvent(FoodEvent event) {
+        logger.warn("Please calculate calorie for: {}", event.getFood().getName());
+    }
+
+}
