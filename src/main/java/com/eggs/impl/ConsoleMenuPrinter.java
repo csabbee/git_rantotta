eb5810b83e0c449e58100c0e2cb8bc312cedfd7d
lalyos <lalyos@yahoo.com>
2013-11-05 14:40:33 +0100

    ConfigSupport introduced to make SPEL more simple

--- src/main/java/com/eggs/impl/ConsoleMenuPrinter.java
+++ src/main/java/com/eggs/impl/ConsoleMenuPrinter.java
@@ -20,7 +20,7 @@ public class ConsoleMenuPrinter extends BaseMenuPrinter implements ApplicationCo
     
     private ApplicationContext ctx;
     
-    @Value("#{ T(java.lang.System).getenv('MENU_LANG') != null ? T(java.lang.System).getenv('MENU_LANG') : T(java.lang.System).getProperties().getProperty('menu.language', 'en') }")
+    @Value("#{T(com.eggs.utils.ConfigurationSupport).getConfig('menu.lang','en')}")
     private Locale locale;
     public ConsoleMenuPrinter() {
         super();
