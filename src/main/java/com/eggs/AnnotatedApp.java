085c21cf61c6bf41e00d7850dccd42c5464092a9
lalyos <lalyos@yahoo.com>
2013-11-06 13:47:46 +0100

    MenuConfiguration introduced

--- src/main/java/com/eggs/AnnotatedApp.java
+++ src/main/java/com/eggs/AnnotatedApp.java
@@ -2,19 +2,16 @@ package com.eggs;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.eggs.configuration.KarcsiConfiguration;
+import com.eggs.configuration.MenuConfiguration;
 
 public class AnnotatedApp {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(KarcsiConfiguration.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuConfiguration.class);
         
-        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
-        for (String name : beanDefinitionNames) {
-            System.out.format("name=%s %n", name);
-        }
-        Restaurant restaurant = ctx.getBean(Restaurant.class);
-        System.out.format("restaurant=%s %n", restaurant);
+        
+        MenuPrinter printer = ctx.getBean(MenuPrinter.class);
+        printer.printMenus();
     }
 
 }
