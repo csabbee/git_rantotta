a4c2897f8b4da9bda9cd4ae17e93c69d655e5d5e
Csaba Valyi <csaba_valyi@epam.com>
2013-11-08 16:10:45 +0100

    @Qualifier annotation added to the CompoundMenuRepository class

--- src/main/java/com/eggs/repo/compound/CompoundMenuRepository.java
+++ src/main/java/com/eggs/repo/compound/CompoundMenuRepository.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.eggs.domain.Menu;
 import com.eggs.interfaces.MenuRepository;
 
 @Component
+@Qualifier("Peti")
 public class CompoundMenuRepository implements MenuRepository{
 
     private List<Menu> menus = new ArrayList<Menu>();
@@ -17,9 +19,7 @@ public class CompoundMenuRepository implements MenuRepository{
     @Autowired
     public CompoundMenuRepository (MenuRepository...repos){
         for (MenuRepository menuRepository : repos) {
-            for (Menu menu : menuRepository.getAllmenu()) {
-                addMenu(menu);
-            }
+            menus.addAll(menuRepository.getAllmenu());
         }
     }
     public List<Menu> getAllmenu() {
