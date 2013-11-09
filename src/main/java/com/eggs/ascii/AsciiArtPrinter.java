281fcaf8bd5bb037da0085f996b97343e71be28e
Csaba Valyi <csaba.valy1@gmail.com>
2013-11-09 12:48:06 +0100

    internationalsing the asciimenuprinter

--- src/main/java/com/eggs/ascii/AsciiArtPrinter.java
+++ src/main/java/com/eggs/ascii/AsciiArtPrinter.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -21,10 +22,12 @@ public class AsciiArtPrinter extends BaseMenuPrinter {
 
     private Logger logger = LoggerFactory.getLogger(AsciiArtPrinter.class);
     
+    @Value("${menu.lang:en}")
+    private Locale locale;
     @Autowired
     private ApplicationContext ctx;
     // shadow,lean,standard,starwars,speed,shadow,nipples,lean,big,
-    private String font = "doom";
+    private String font ="doom";
 
     @Autowired
     public AsciiArtPrinter(MenuRepository... menuRepository) {
@@ -42,8 +45,8 @@ public class AsciiArtPrinter extends BaseMenuPrinter {
         
         System.out.println(menu.getRestaurant().getAddress());
         
-        String nameHeader = ctx.getMessage("header.name", new Object[0], Locale.getDefault());
-        String priceHeader = ctx.getMessage("header.price", new Object[0],  Locale.getDefault());
+        String nameHeader = ctx.getMessage("header.name", new Object[0], this.locale);
+        String priceHeader = ctx.getMessage("header.price", new Object[0],  this.locale);
 
         System.out.format(" %-20s | %10s %n", nameHeader, priceHeader);
         
