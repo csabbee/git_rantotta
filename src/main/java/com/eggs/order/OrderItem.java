18ed672178f16f346ae5772d5fe348fe3b0cd961
Csaba Valyi <csaba.valy1@gmail.com>
2013-11-10 16:04:52 +0100

    Introducing the OrderItem class

--- src/main/java/com/eggs/order/OrderItem.java
+++ src/main/java/com/eggs/order/OrderItem.java
@@ -3,15 +3,18 @@ package com.eggs.order;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.stereotype.Component;
 /**
  * This class represents an item in an order
  * @author Csaba_Valyi
  *
  */
+@Component
 public class OrderItem {
     private int quantity;
     private String foodId;
     
+    public OrderItem(){}
     public OrderItem(String foodId, int quantity) {
         this.quantity = quantity;
         this.foodId = foodId;
@@ -30,6 +33,11 @@ public class OrderItem {
     }
     public void setFoodId(String foodId) {
         this.foodId = foodId;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem [quantity=" + quantity + ", foodId=" + foodId + "]";
     }
     
 
