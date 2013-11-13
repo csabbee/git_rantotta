723d1656e16103caf29f2d9f1a19a1966f3bda78
Csaba Valyi <csaba_valyi@epam.com>
2013-11-13 09:33:58 +0100

    [refactor] removed the unnecessary methods from OrderTaker

--- src/main/java/com/eggs/order/OrderTaker.java
+++ src/main/java/com/eggs/order/OrderTaker.java
@@ -6,11 +6,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.eggs.api.MenuRepository;
-import com.eggs.configuration.OrderConfiguration;
 /**
  * This class is for to take orders and print them on the console
  * @author Csaba_Valyi
@@ -28,18 +26,5 @@ public class OrderTaker {
         for (MenuRepository menuRepository : menuRepo) {
             repos.add(menuRepository);
         }
-    }
-
-    private void printOrders(){
-        for (OrderInstance orderinstance : orders) {
-            System.out.format("orderinstance=%s", orderinstance);
-        }
-    }
-    
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(OrderConfiguration.class);
-        
-        OrderInstance oi = ctx.getBean(OrderInstance.class);
-        System.out.format("oi=%s", oi);
     }
 }
