7a0a48a2056d9f362710d70490e55b534b2599b7
Csaba Valyi <csaba_valyi@epam.com>
2013-11-13 11:44:07 +0100

    OrderTaker prints out menus from the menurepositories

--- src/main/java/com/eggs/order/OrderTaker.java
+++ src/main/java/com/eggs/order/OrderTaker.java
@@ -3,12 +3,16 @@ package com.eggs.order;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.eggs.api.MenuRepository;
+import com.eggs.domain.Menu;
+import com.eggs.repo.order.OrderRepository;
 /**
  * This class is for to take orders and print them on the console
  * @author Csaba_Valyi
@@ -17,14 +21,29 @@ import com.eggs.api.MenuRepository;
 @Component
 public class OrderTaker {
 
-    private List<OrderInstance> orders = new ArrayList<OrderInstance>();
+    //private List<OrderInstance> orders = new ArrayList<OrderInstance>();
     private Logger logger = LoggerFactory.getLogger(OrderTaker.class);
     private List<MenuRepository> repos = new ArrayList<MenuRepository>();
+    @Autowired
+    private OrderRepository orderrepo;
     
     @Autowired
     public OrderTaker(MenuRepository... menuRepo){
         for (MenuRepository menuRepository : menuRepo) {
             repos.add(menuRepository);
         }
+    }
+
+    @PostConstruct
+    public void printMenus(){
+        for (MenuRepository repo : repos) {
+            for (Menu menu : repo.getAllmenu()) {
+                System.out.format("%s %n", menu);
+            }
+        }
+    }
+    
+    public void addOrder(OrderInstance orderinstance){
+        orderrepo.getOrderrepo().add(orderinstance);
     }
 }
