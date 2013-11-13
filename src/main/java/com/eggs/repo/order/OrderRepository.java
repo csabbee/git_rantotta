8f0f7085035509f91fca43f1663cf528f598108a
Csaba Valyi <csaba_valyi@epam.com>
2013-11-13 15:49:01 +0100

    OrderRepository class introduced

---
+++ src/main/java/com/eggs/repo/order/OrderRepository.java
@@ -0,0 +1,38 @@
+package com.eggs.repo.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import com.eggs.order.OrderInstance;
+
+@Component
+@Lazy
+public class OrderRepository {
+
+    private List<OrderInstance> orderrepo = new ArrayList<OrderInstance>();
+    private final Logger logger = LoggerFactory.getLogger(OrderRepository.class);
+    
+    @Autowired
+    public OrderRepository(OrderInstance... orderinstances){
+        for (OrderInstance orderInstance : orderinstances) {
+            orderrepo.add(orderInstance);
+            logger.debug(orderInstance.toString());
+        }
+    }
+
+    public List<OrderInstance> getOrderrepo() {
+        return orderrepo;
+    }
+
+    public void setOrderrepo(List<OrderInstance> orderrepo) {
+        this.orderrepo = orderrepo;
+    }
+    
+    
+}
