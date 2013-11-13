8e9aec397f6687a78ea2f11ed3647378d74244c6
Csaba Valyi <csaba_valyi@epam.com>
2013-11-13 11:45:27 +0100

    @Lazy annotation added to OrderPrinter and @Autowired to the constructor

--- src/main/java/com/eggs/order/OrderPrinter.java
+++ src/main/java/com/eggs/order/OrderPrinter.java
@@ -1,9 +1,19 @@
 package com.eggs.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import com.eggs.repo.order.OrderRepository;
+
 @Component
+@Lazy
 public class OrderPrinter {
 
+    private OrderRepository orderrepo;
     
+    @Autowired
+    public OrderPrinter(OrderRepository orderrepo){
+        this.orderrepo = orderrepo;
+    }
 }
