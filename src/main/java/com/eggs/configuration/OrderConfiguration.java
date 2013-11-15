f133fb72dc80039b7a0c641eda1cbf8ad526097b
Csaba Valyi <csaba_valyi@epam.com>
2013-11-15 17:09:26 +0100

    [refactor] unnecessary basePackageClasses removed from @ComponentScan

--- src/main/java/com/eggs/configuration/OrderConfiguration.java
+++ src/main/java/com/eggs/configuration/OrderConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("order")
 @ComponentScan(basePackageClasses={com.eggs.order.OrderTaker.class, 
-                                   com.eggs.repo.ComponentScanHelper.class,
-                                   com.eggs.domain.AnnotatedApp.class})
+                                   com.eggs.repo.ComponentScanHelper.class})
 public class OrderConfiguration {
     
 }
