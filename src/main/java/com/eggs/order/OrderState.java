a3e49a560adfca4c22a82dc7cad5d5e87430585f
Csaba Valyi <csaba_valyi@epam.com>
2013-11-15 17:11:39 +0100

    [refactor] now following the ENUM conventions and toString() method is overriden

--- src/main/java/com/eggs/order/OrderState.java
+++ src/main/java/com/eggs/order/OrderState.java
@@ -2,17 +2,18 @@ package com.eggs.order;
 
 public enum OrderState {
 
-    indelivery("IN_DELIVERY"),
-    pending("PENDING"),
-    delivered("DELIVERED"),
-    closed("CLOSED");
+    IN_DELIVERY("IN_DELIVERY"),
+    PENDING("PENDING"),
+    DELIVERED("DELIVERED"),
+    CLOSED("CLOSED");
     
     private final String state;
     
     private OrderState(String state){
         this.state = state;
     }
-    public String getState() {
+    @Override
+    public String toString(){
         return state;
     }
 }
